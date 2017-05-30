8cd649111e25ab59145eccb80f9a189e70315fce
henryzhao <zhaoyuqi54@gmail.com>
2017-05-30 20:02:08 +0800

    大步更新

--- assets/js/books.js
+++ assets/js/books.js
@@ -1,3 +1,51 @@
 /**
  * Created by Angel on 2017/5/30.
  */
+
+var vm = new Vue({
+
+    el:"#home_table",
+    data:{
+        booksNumber:''
+    },
+    methods:{
+
+    },
+    mounted: function() {
+        alert('a');
+        this.$nextTick(function () {
+            //alert('b');
+            this.$http.get('http://software.whu.edu.cn:8080/quick4j/rest/books/number').then(function(res) {
+                console.log(res.data);
+                  alert('c');
+                this.booksNumber = res.data;
+            }).catch(function(err) {
+                console.log('fail' + status + "," + request);
+                console.log(err);
+            })
+        })
+    },
+
+
+});
+// function newOrders(orders, queryParams){
+//
+//     var newOrders = [];
+//     // alert('a');
+//     if(queryParams.tel===""){
+//         // alert('b');
+//         return orders;
+//     }
+//     // alert('c');
+//     $.each(orders,function(index, value){
+//         // alert('d');
+//         if(value.tel===queryParams.tel){
+//             // alert('e');
+//             //满足条件的push进返回数组
+//             newOrders.push(value);
+//         }
+//     });
+//
+//     return newOrders;
+//
+// }
